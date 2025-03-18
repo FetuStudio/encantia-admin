6231badfd0b4ccaf2536aa98df75417c74000fa6
FetuStudio <contacto@encantia.lat>
2025-03-18 18:16:50 +0100

    Update notes.js

--- pages/notes.js
+++ pages/notes.js
@@ -14,6 +14,8 @@ export default function Navbar() {
   const [imageUrl, setImageUrl] = useState(null);  
   const [userProfile, setUserProfile] = useState(null);
   const [userRole, setUserRole] = useState(null); // Estado para almacenar el rol del usuario
+  const [showMessage, setShowMessage] = useState(false); // Agregar estado para el mensaje
+  const [userMessage, setUserMessage] = useState(""); // Mensaje que se mostrará
   const router = useRouter();
 
   // Función para obtener las notas y perfil del usuario
@@ -29,10 +31,10 @@ export default function Navbar() {
         }
 
         if (user) {
-          // Obtener las notas del usuario
+          // Obtener las notas del usuario, incluyendo la columna 'mensaje'
           const { data: notesData, error: notesError } = await supabase
             .from("ntas")
-            .select("evau1, evau2, evau3, evau4, evau5, nm, evaucat")
+            .select("evau1, evau2, evau3, evau4, evau5, nm, evaucat, urod, mensaje")
             .eq("user_id", user.id);
 
           if (notesError) {
@@ -86,6 +88,40 @@ export default function Navbar() {
     setSelectedCategory(event.target.value);
   };
 
+  // Nueva función para manejar el click en el mensaje
+  const handleMessageClick = () => {
+    if (userNotes && userNotes.mensaje) {
+      setUserMessage(userNotes.mensaje); // Asignar el mensaje de la columna 'mensaje' al estado
+    }
+    setShowMessage(true); // Muestra el mensaje cuando se hace clic
+  };
+
+  const MessageModal = ({ message, onClose }) => {
+    return (
+      <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50">
+        <div className="bg-gray-900 p-6 rounded-lg shadow-lg text-center w-96">
+          {/* Título del mensaje en grande */}
+          <div className="text-2xl font-semibold text-white mb-4">
+            Mensaje de los owners
+          </div>
+    
+          {/* Contenido del mensaje */}
+          <div className="text-white">{message}</div>
+    
+          {/* Botón de cerrar */}
+          <button
+            onClick={onClose}
+            className="absolute top-2 right-2 text-white text-4xl font-bold"
+          >
+            ×
+          </button>
+        </div>
+      </div>
+    );
+  };
+  
+  
+
   const evaluationNames = {
     evau1: "Atención al Cliente",
     evau2: "Comportamiento",
@@ -93,6 +129,7 @@ export default function Navbar() {
     evau4: "Trabajo en Equipo",
     evau5: "Experiencia",
     nm: "Nota Media",
+    urod: "Resultado Fina (Ascender o Descender)",
   };
 
   const getFilteredNotes = () => {
@@ -122,6 +159,15 @@ export default function Navbar() {
               return null;
             })}
           </div>
+
+          {/* Imagen de mensaje debajo del texto de la categoría */}
+          <div className="inline-block cursor-pointer mt-4" onClick={handleMessageClick}>
+            <img
+              src="https://images.encantia.lat/mpend.png"
+              alt="Mensaje"
+              className="w-15 h-15 ml-2"
+            />
+          </div>
         </div>
       );
     }
@@ -129,124 +175,113 @@ export default function Navbar() {
   };
 
   return (
-    <div className="flex flex-col h-screen p-4 bg-gray-900 text-white relative">
-      <Head>
-        <title>NOTAS</title>
-      </Head>
-
-      <div className="flex justify-between items-center mb-4">
-        <div>
-          <img src="https://images.encantia.lat/encantia-logo-2025.webp" alt="Logo" className="h-16" />
-        </div>
-
-        <div className="flex gap-4">
-        <button
-                        onClick={() => router.push("/")}
-                        className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
-                    >
-                        Inicio
-                    </button>      
-                    <button
-                        onClick={() => router.push("/notes")}
-                        className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
-                    >
-                        Notas
-                    </button> 
-                    <button
-                        onClick={() => router.push("/bdm")}
-                        className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
-                    >
-                        Buzon de mensajes
-                    </button> 
-                    <button
-                        onClick={() => router.push("/advert")}
-                        className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
-                    >
-                        Advertencias
-                    </button> 
-                    <button
-                        onClick={() => router.push("/projects")}
-                        className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
-                    >
-                        Proyectos
-                    </button> 
-                    <button
-                        onClick={() => router.push("/cprojects")}
-                        className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors"
-                    >
-                        Crear Proyectos
-                    </button> 
-        </div>
-
-        <div className="relative">
-          <img
-            src={imageUrl}  
-            alt="Avatar"
-            className="w-12 h-12 rounded-full cursor-pointer"
-            onClick={toggleMenu}
-          />
-          {showMenu && (
-            <div className="absolute right-0 mt-2 w-48 bg-gray-800 text-white rounded-lg shadow-lg z-10">
-              <ul className="py-2">
-                <li className="px-4 py-2 text-white cursor-pointer hover:bg-gray-700" onClick={() => router.push(`/profile/${userProfile.user_id}`)}>
-                  Ver Perfil
-                </li>
-                <li className="px-4 py-2 text-red-500 cursor-pointer hover:bg-gray-700" onClick={handleLogout}>
-                  Cerrar Sesión
-                </li>
-              </ul>
-            </div>
-          )}
-        </div>
-      </div>
-
-      <div className="flex justify-center items-center mb-6 text-white font-bold text-3xl">NOTAS</div>
-
-      {loading && <div className="text-white">Estamos cargando tus notas, por favor espera...</div>}
-      {error && <div className="text-red-500">{error}</div>}
-
-      {!loading && !error && (
-        <div className="flex justify-end mb-4">
-          <label className="text-white mr-2 text-xl" htmlFor="categorySelect">
-            <strong>Seleccionar Categoría:</strong>
-          </label>
-          <select
-            id="categorySelect"
-            value={selectedCategory}
-            onChange={handleCategorySelect}
-            className="bg-gray-800 text-white px-4 py-2 rounded-lg shadow-md w-56 text-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-          >
-            <option value="">-- Elige una categoría --</option>
-            {categories.map((category, index) => (
-              <option key={index} value={category} className="bg-gray-700">
-                {category}
-              </option>
-            ))}
-          </select>
-        </div>
+    <div className="relative">
+      {/* Mostrar el mensaje en un modal */}
+      {showMessage && (
+        <MessageModal message={userMessage} onClose={() => setShowMessage(false)} />
       )}
+      
+      <div className="flex flex-col h-screen p-4 bg-gray-900 text-white relative">
+        <Head>
+          <title>NOTAS</title>
+        </Head>
 
-      <div className="text-white mt-4">{getFilteredNotes()}</div>
+        <div className="flex justify-between items-center mb-4">
+          <div>
+            <img src="https://images.encantia.lat/encantia-logo-2025.webp" alt="Logo" className="h-16" />
+          </div>
 
-      {showLogoutModal && (
-        <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50">
-          <div className="bg-gray-900 p-6 rounded-lg shadow-lg text-center">
-            <p className="text-lg mb-4">¿Estás seguro de que deseas cerrar sesión?</p>
-            <div className="flex justify-center gap-4">
-              <button onClick={confirmLogout} className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-400">
-                Sí
-              </button>
-              <button onClick={cancelLogout} className="px-4 py-2 bg-gray-300 text-black rounded hover:bg-gray-200">
-                No
-              </button>
-            </div>
+          <div className="flex gap-4">
+            <button onClick={() => router.push("/")} className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors">
+              Inicio
+            </button>
+            <button onClick={() => router.push("/notes")} className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors">
+              Notas
+            </button>
+            <button onClick={() => router.push("/bdm")} className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors">
+              Buzon de mensajes
+            </button>
+            <button onClick={() => router.push("/advert")} className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors">
+              Advertencias
+            </button>
+            <button onClick={() => router.push("/projects")} className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors">
+              Proyectos
+            </button>
+            <button onClick={() => router.push("/cprojects")} className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 transition-colors">
+              Crear Proyectos
+            </button>
+          </div>
+
+          <div className="relative">
+            <img
+              src={imageUrl}
+              alt="Avatar"
+              className="w-12 h-12 rounded-full cursor-pointer"
+              onClick={toggleMenu}
+            />
+            {showMenu && (
+              <div className="absolute right-0 mt-2 w-48 bg-gray-800 text-white rounded-lg shadow-lg z-10">
+                <ul className="py-2">
+                  <li className="px-4 py-2 text-white cursor-pointer hover:bg-gray-700" onClick={() => router.push(`/profile/${userProfile.user_id}`)}>
+                    Ver Perfil
+                  </li>
+                  <li className="px-4 py-2 text-red-500 cursor-pointer hover:bg-gray-700" onClick={handleLogout}>
+                    Cerrar Sesión
+                  </li>
+                </ul>
+              </div>
+            )}
           </div>
         </div>
-      )}
 
-      {/* Pie de página con el copyright */}
-      <div className="mt-8 p-4 bg-gray-900 text-center text-sm text-gray-400 fixed bottom-0 w-full">
-        <p>© 2025 Encantia. Todos los derechos reservados.</p>
+        <div className="flex justify-center items-center mb-6 text-white font-bold text-3xl">NOTAS</div>
+
+        {loading && <div className="text-white">Estamos cargando tus notas, por favor espera...</div>}
+        {error && <div className="text-red-500">{error}</div>}
+
+        {!loading && !error && (
+          <div className="flex justify-end mb-4">
+            <label className="text-white mr-2 text-xl" htmlFor="categorySelect">
+              <strong>Seleccionar Categoría:</strong>
+            </label>
+            <select
+              id="categorySelect"
+              value={selectedCategory}
+              onChange={handleCategorySelect}
+              className="bg-gray-800 text-white px-4 py-2 rounded-lg shadow-md w-56 text-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+            >
+              <option value="">-- Elige una categoría --</option>
+              {categories.map((category, index) => (
+                <option key={index} value={category} className="bg-gray-700">
+                  {category}
+                </option>
+              ))}
+            </select>
+          </div>
+        )}
+
+        <div className="text-white mt-4">{getFilteredNotes()}</div>
+
+        {showLogoutModal && (
+          <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50">
+            <div className="bg-gray-900 p-6 rounded-lg shadow-lg text-center">
+              <p className="text-lg mb-4">¿Estás seguro de que deseas cerrar sesión?</p>
+              <div className="flex justify-center gap-4">
+                <button onClick={confirmLogout} className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-400">
+                  Sí
+                </button>
+                <button onClick={cancelLogout} className="px-4 py-2 bg-gray-300 text-black rounded hover:bg-gray-200">
+                  No
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+
+        {/* Pie de página con el copyright */}
+        <div className="mt-8 p-4 bg-gray-900 text-center text-sm text-gray-400 fixed bottom-0 w-full">
+          <p>© 2025 Encantia. Todos los derechos reservados.</p>
+        </div>
       </div>
     </div>
   );
